6c7f0cc695b72151bd426ceb7c4e4e3b56b60089
ewpac <ew.pach@gmail.com>
2022-09-20 19:49:05 +0200

    Added for loops

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // For loop
+        String[] cars=new String[]{"Ford", "Opel", "Audi", "Mercedes", "Toyota"};
+        for (int i=0; i<5; i++){
+            System.out.println(cars[i]);
+        }
     }
 }
